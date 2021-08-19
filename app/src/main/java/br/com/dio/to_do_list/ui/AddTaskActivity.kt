91e152169a38d1bc9cb6738a8bb6f65579fe02d5
Felipe <felipkv@hotmail.com>
2021-08-19 11:04:42 -0300

    created viewbind for AddTaskActivity and date picker

---
+++ app/src/main/java/br/com/dio/to_do_list/ui/AddTaskActivity.kt
@@ -0,0 +1,36 @@
+package br.com.dio.to_do_list.ui
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.dio.to_do_list.databinding.ActivityAddTaskBinding
+import br.com.dio.to_do_list.extensions.format
+import br.com.dio.to_do_list.extensions.text
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.util.*
+
+class AddTaskActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddTaskBinding
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        val binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.tilDate.editText?.setOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker().build()
+            datePicker.addOnPositiveButtonClickListener {
+                val timeZone = TimeZone.getDefault()
+                val offset = timeZone.getOffset(Date().time) * -1
+                binding.tilDate.text = Date(it + offset).format()
+            }
+            datePicker.show(supportFragmentManager, "DATE_PICKER_TAG")
+        }
+    }
+
+}
